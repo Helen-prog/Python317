82b2f0fa06fc162f630ec5cec1b93e3573a42ec7
Helen <elena.kozovyakina@gmail.com>
2024-05-22 21:25:20 +0300

    db

--- main.py
+++ main.py
@@ -6461,7 +6461,7 @@
 #     run()
 
 
-import sqlite3
+# import sqlite3
 
 
 # con = sqlite3.connect("profile.db")
@@ -6485,52 +6485,93 @@ import sqlite3
 #     DROP TABLE person_table
 #     """)
 
-    # cur.execute("""
-    # ALTER TABLE person_table
-    # DROP COLUMN home_address
-    # """)
+# cur.execute("""
+# ALTER TABLE person_table
+# DROP COLUMN home_address
+# """)
 
-    # cur.execute("""
-    # ALTER TABLE person_table
-    # RENAME COLUMN address TO home_address
-    # """)
+# cur.execute("""
+# ALTER TABLE person_table
+# RENAME COLUMN address TO home_address
+# """)
 
-    # cur.execute("""
-    # ALTER TABLE person_table
-    # ADD COLUMN address_add TEXT NOT NULL DEFAULT "Москва";
-    # """)
+# cur.execute("""
+# ALTER TABLE person_table
+# ADD COLUMN address_add TEXT NOT NULL DEFAULT "Москва";
+# """)
 
-    # cur.execute("""
-    # ALTER TABLE person
-    # RENAME TO person_table;
-    # """)
+# cur.execute("""
+# ALTER TABLE person
+# RENAME TO person_table;
+# """)
 
-    # cur.execute("""
-    # CREATE TABLE IF NOT EXISTS person(
-    # id INTEGER PRIMARY KEY AUTOINCREMENT,
-    # name TEXT NOT NULL,
-    # phone BLOB NOT NULL DEFAULT "+79099000000",
-    # age INTEGER CHECK(age > 0 AND age < 100),
-    # email TEXT UNIQUE
-    # )""")
+# cur.execute("""
+# CREATE TABLE IF NOT EXISTS person(
+# id INTEGER PRIMARY KEY AUTOINCREMENT,
+# name TEXT NOT NULL,
+# phone BLOB NOT NULL DEFAULT "+79099000000",
+# age INTEGER CHECK(age > 0 AND age < 100),
+# email TEXT UNIQUE
+# )""")
 
 
-with sqlite3.connect("db_4.db") as con:
+# with sqlite3.connect("db_4.db") as con:
+#     cur = con.cursor()
+#
+#     cur.execute("""
+#     SELECT *
+#     FROM Ware
+#     ORDER BY Price DESC
+#     LIMIT 2, 5
+#     """)
+#
+#     # res = cur.fetchall()  # [(), ()]
+#     # res = cur.fetchone()  # ()
+#     # print(res)
+#     # res = cur.fetchmany(3)  # [()]
+#     # print(res)
+#
+#     for res in cur:
+#         print(res)
+
+
+import sqlite3
+
+cars_list = [
+    ('BMW', 54000),
+    ('Chevrolet', 46000),
+    ('Daewoo', 38000),
+    ('Citroen', 29000),
+    ('Honda', 33000),
+]
+
+with sqlite3.connect("car.db") as con:
     cur = con.cursor()
-
     cur.execute("""
-    SELECT *
-    FROM Ware
-    ORDER BY Price DESC
-    LIMIT 2, 5 
+    CREATE TABLE IF NOT EXISTS cars (
+        car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        model TEXT,
+        price INTEGER
+    )
     """)
 
-    # res = cur.fetchall()  # [(), ()]
-    # res = cur.fetchone()  # ()
-    # print(res)
-    # res = cur.fetchmany(3)  # [()]
-    # print(res)
+    cur.executescript("""
+    DELETE FROM cars WHERE model LIKE 'B%';
+    UPDATE cars SET price = price + 100;
+    """)
 
-    for res in cur:
-        print(res)
+    # cur.execute(f"UPDATE cars SET price = :Price where model LIKE 'B%'", {'Price': 0})
 
+    # cur.executemany("INSERT INTO cars VALUES(NULL, ?, ?)", cars_list)
+
+    # for car in cars_list:
+    #     cur.execute("INSERT INTO cars VALUES(NULL, ?, ?)", car)
+
+    # cur.execute("INSERT INTO cars VALUES(1, 'Renault', 22000)")
+    # cur.execute("INSERT INTO cars VALUES(2, 'Volvo', 29000)")
+    # cur.execute("INSERT INTO cars VALUES(3, 'Mercedes', 57000)")
+    # cur.execute("INSERT INTO cars VALUES(4, 'Bentley', 35000)")
+    # cur.execute("INSERT INTO cars VALUES(5, 'Audi', 52000)")
+
+# con.commit()
+# con.close()
