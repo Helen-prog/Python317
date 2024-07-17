60d30855e3079012b46e975a515541854195ba0f
Helen <elena.kozovyakina@gmail.com>
2024-07-17 21:26:28 +0300

    auth

--- fourth/devsearch/users/urls.py
+++ fourth/devsearch/users/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('profile/<str:pk>/', views.user_profile, name="user_profile"),
     path('login/', views.login_user, name="login"),
     path('logout/', views.logout_user, name="logout"),
+    path('register/', views.register_user, name="register"),
+    path('account/', views.user_account, name="account"),
 ]
