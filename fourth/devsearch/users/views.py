cfb339c16e2b4f3b18c8e1fd11288b60844a0727
Helen <elena.kozovyakina@gmail.com>
2024-08-19 22:10:36 +0300

    blog

--- fourth/devsearch/users/views.py
+++ fourth/devsearch/users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 from django.contrib.auth.decorators import login_required
 from .utils import search_profiles, paginate_profiles
 
@@ -176,3 +176,33 @@ def view_message(request, pk):
         message.save()
     context = {'message': message}
     return render(request, "users/message.html", context)
+
+
+def create_message(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+
+            messages.success(request, 'Your message was successfully sent!')
+            return redirect('user_profile', pk=recipient.id)
+    context = {
+        'recipient': recipient,
+        'form': form
+    }
+    return render(request, 'users/message_form.html', context)
