4aaab3a731c0ea8efdbd3f32dfc82a4b2ec354ab
Helen <elena.kozovyakina@gmail.com>
2024-08-28 21:33:26 +0300

    blog

--- fifth/programming_blog/blog/forms.py
+++ fifth/programming_blog/blog/forms.py
@@ -3,6 +3,7 @@ from .models import *
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from captcha.fields import CaptchaField
 
 
 class RegisterUserForm(UserCreationForm):
@@ -29,3 +30,11 @@ class AddPostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['cat'].empty_label = "Категория не выбрана"
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Имя', max_length=255)
+    email = forms.EmailField(label='Email')
+    content = forms.CharField(label='Сообщение', widget=forms.Textarea(attrs={'cols': 60, 'rows': 7}))
+    captcha = CaptchaField()
+
